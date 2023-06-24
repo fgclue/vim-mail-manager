3e9c88903c2787b05586e7dd1b01a2a46903a43a
fgclue <schefferdaudtfelipe@gmail.com>
2023-06-23 22:38:18 -0300

    cooooomit

---
+++ main.c
@@ -0,0 +1,15 @@
+// __    __ _  __________ __________ ______ _  _
+// \ \  / //_// __  __  // __  __  // __  //_// /
+//  \ \/ // // / / / / // / / / / // /_/  \\ \\ \
+//   \__//_//_/ /_/ /_//_/ /_/ /_//_____/\_\\_\\_\
+//    vim mail manager 
+//    vmm.biitle.nl
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Hello, ");
+	printf(getenv("USER"));
+	printf("!\n");
+}
